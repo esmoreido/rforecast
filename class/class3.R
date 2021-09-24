92ed5c8b7d8660688695edf8ce8458c4a4100dd2
esmoreido <moreido@mail.ru>
2021-09-24 12:50:46 +0300

    modified class 3

--- class/class3.R
+++ class/class3.R
@@ -1,11 +1,11 @@
 Sys.setlocale("LC_ALL","Russian")
-library(readxl)
 
 # произвольный датафрейм
 df <- data.frame(obs = rnorm(10),
                  pred = rnorm(10))
 # новый столбец - последовательность дат
-newcol <- seq.Date(from = as.Date('2021-09-21'), by = '1 day', length.out = 10)
+newcol <- seq.Date(from = as.Date('2021-09-21'), 
+                   by = '1 day', length.out = 10)
 newcol
 # добавляем к дф
 df <- cbind(df, newcol)
@@ -21,9 +21,14 @@ newrow <- list(rnorm(1), rnorm(1), '2021-10-01')
 df <- rbind(df, newrow)
 
 df$obs <- as.numeric(df$obs)
+df$pred <- as.numeric(df$pred)
+
+df <- df[-12,]
 
 # читаем данные из excel
-df <- read_xlsx('data/oka.xlsx')
+# install.packages("readxl")
+library(readxl)
+df <- read_xlsx('data/oka/oka.xlsx')
 summary(df)
 head(df, 10)
 tail(df, 10)
@@ -42,40 +47,86 @@ hist(df$area, freq = F)
 hist(df$area, plot = F)
 # факторы
 plot(x = df$side, y = df$area)
-df$side <- factor(df$side)
+clean_df$side <- factor(clean_df$side)
 levels(df$side)
 plot(x = df$side, y = df$area)
 # создание факторов разбиением
 df$size <- cut(df$area, labels = c('малая','средняя','крупная'), 
-               breaks = c(0, 3500, 10000, 100000), ordered_result = T)
+               breaks = c(0, 3500, 10000, 100000), ordered_result = TRUE)
 plot(x = df$size, y = df$len)
+
+
+
 # графика ggplot
 library(ggplot2)
+
+ggplot(data = df, aes(x = dist, y = len)) +
+  geom_point() + geom_line()
+
+ggplot(data = clean_df, aes(x = dist, y = len)) +
+  geom_point() + geom_line()
+
+ggplot(data = clean_df, aes(x = len, y = area, col=side)) +
+  geom_point(size=7)
+
+ggplot(data = na.omit(df), aes(x = dist, y = len, col=size)) +
+  geom_point(size=7) 
+
+ggplot(data = na.omit(df), aes(x = dist, y = len, col=size)) +
+  geom_point(size=7) + 
+  geom_text(aes(label=name), col='Black', nudge_x = 50)
+
+ggplot(data = na.omit(df), aes(x = dist, y = len, col=size)) +
+  geom_point(size=7) + 
+  geom_text(aes(label=name), col='Black', nudge_x = 50, 
+            family = 'serif') + 
+  labs(title = 'Притоки р. Ока', x = 'Расстояние от устья, км',
+       y = 'Длина реки, км', col = 'Размер') + 
+  theme_minimal(base_size = 20) +
+  theme(text = element_text(family = 'serif'))
+
+ggplot(na.omit(df), aes(x = len, fill=side)) + 
+  geom_histogram(binwidth = 200, position = 'dodge')
+
+ggplot(na.omit(df), aes(x = area, fill=side)) + 
+  geom_histogram(binwidth = 20000, position = 'dodge')
+
 ggplot(df, aes(x=size, y=len, col=size)) + geom_boxplot() + 
   stat_boxplot(geom = 'errorbar') 
-ggplot(df, aes(x=area, fill=side)) + geom_histogram(binwidth = 10000, position = 'dodge')
+ggplot(df, aes(x=area, fill=side)) + 
+  geom_histogram(binwidth = 10000, position = 'dodge')
 ggplot(df, aes(x=len, y=area, col=side)) + geom_point(size=5)
+
 ggplot(df, aes(x=len, y=area)) + geom_point(size=5) + 
   geom_smooth(method = 'lm', formula = y ~ x, se = F)
+
+ggplot(df, aes(x=len, y=area)) + geom_point(size=5) + 
+  geom_smooth()
+
 # линейная аппроксимация
 area_model <- lm(data = df, formula = area ~ len)
-df$pred_area <- predict(area_model)
+area_model
 
-ggplot(df, aes(x=len)) + geom_point(aes(y=area, col='Факт'), size=5) +
-  geom_line(aes(y=pred_area, col='Модель'), size=5) +
-  geom_text(aes(x = 100, y=40000, label=))
+clean_df$pred_area <- predict(area_model)
 
+ggplot(clean_df, aes(x=len)) + 
+  geom_point(aes(y = area, col='Факт'), size=8) + 
+  geom_line(aes(y = pred_area, col='Модель'), size=3)
+coef(area_model)[1]
+cor(clean_df$area, clean_df$pred_area)
 coef_a <- as.character(round(coef(area_model)[2], 2))
 coef_b <- as.character(round(coef(area_model)[1], 2))
-cor_coef <- as.character(round(cor(df$area, df$pred_area), 2))
+cor_coef <- as.character(round(cor(clean_df$area, clean_df$pred_area), 2))
 
-model_text <- paste("y = ", coef_a, " * x ", coef_b, ", R = ", cor_coef)
+model_text <- paste0("y = ", coef_a, " * x ", coef_b, ", R = ", cor_coef)
 model_text
 
-p <- ggplot(df, aes(x=len)) + geom_point(aes(y=area, col='Факт'), size=5) +
+p <- ggplot(clean_df, aes(x=len)) + geom_point(aes(y=area, col='Факт'), size=5) +
   geom_line(aes(y=pred_area, col='Модель'), size=5) +
   geom_text(aes(x = 100, y=40000, label=model_text))
 p
 ggsave(plot = p, filename = 'linear_model.png', device = 'png', 
-       width = 10, height = 8, units = 'in', dpi = 300)
+       width = 10, height = 8, units = 'cm', dpi = 100)
+
+
 
