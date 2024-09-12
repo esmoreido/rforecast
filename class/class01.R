6e139cc612b41cb0b44b58f155419ff41ff5e8f7
esmoreido <moreido@mail.ru>
2024-09-12 22:30:44 +0300

    class and data updates

--- class/class01.R
+++ class/class01.R
@@ -1,3 +1,4 @@
+# устанавливаем поддержку кириллицы
 Sys.setlocale(category = "LC_ALL", locale = "Russian")
 # сложение
 2 + 6
@@ -13,30 +14,56 @@ Sys.setlocale(category = "LC_ALL", locale = "Russian")
 sqrt(9) 
 #возведение в степень
 3 ^ 2
+# сравнение
+3 > 5
+3 < 6
 # присвоение переменной значения: числовое
 x <- 2
 y = 3.5
 x + y 
+z <- x + y
+
 # текстовое
 me <- "Сева"
 val <- 10
 
-ls <- 1
-
 # вектор текстовый
-students <- c("Вася", "Петя", "Маша", "Коля", "Женя", "Эдуард")
+students <- c("Вася", "Петя", "Маша", 
+              "Коля", "Женя", "Эдуард")
+students
+str(students)
 length(students)
 class(students)
+typeof(students)
 # числовой вектор
-height <- c(180, 185, 170, 182, 168, 198)
+height <- c(180, 185, 170, 
+            182, 168, 198)
 length(height)
+class(height)
+# вектор дат
+today <- Sys.Date()
+today
+class(today)
+typeof(today)
+unclass(today)
+
+# объекты в памяти
 ls()
+# удаление объектов из памяти
 rm(students)
 rm(list = ls())
-# именованный вектор
-names(height) <- students
-height
-names(height)
+
+# приведение типов
+num <- c(1, 4, 7)
+nom <- c(1, 4, '7')
+typeof(num)
+typeof(nom)
+nom <- as.integer(nom)
+typeof(nom)
+str(nom)
+nas_conv <- c(1, 4, 'семь')
+nas_conv <- as.integer(nas_conv)
+
 # обращение к элементу
 height[1]
 h1 <- height[1 : 3] + 20
@@ -44,12 +71,18 @@ height[-1]
 height[-1:-3]
 height[c(-1,-3)]
 
+# именованный вектор
+names(height) <- students
+height
+names(height)
+
 # простые статистики
 max(height)
 min(height)
 mean(height)
 var(height)
 sd(height)
+summary(height)
 # сортировка
 sort(height)
 sort(height, decreasing = TRUE)
@@ -65,31 +98,21 @@ typeof(mask_180)
 height[mask_180]
 height[height <= 180]
 
-# приведение типов
-num <- c(1, 4, 7)
-nom <- c(1, 4, '7')
-typeof(num)
-typeof(nom)
-nom <- as.integer(nom)
-typeof(nom)
-
-# цикл по элементам вектор
+# цикл по элементам вектора
 for (i in students){
-  print(i)
+  print(paste("Кто это? Это", i))
 }
 for (k in height) {
   print(k + 100)
 }
 
-# apply
-sapply(height, FUN = function(x) x+2)
-
 # функция
-maxval <- function(x){
-  mv <- sort(x, decreasing = T)[1:3]
-  print(mv)
-} 
-maxval(height)
+kelvin_to_celsius <- function(temp){
+  celsius <- temp - 273.15
+  return(celsius)
+}
+
+kelvin_to_celsius(temp = 285.3)
 
 # сделать матрицу 3х3, последовательно заполненную числами от 1 до 9
 matrix(1:9)
@@ -99,13 +122,23 @@ mat <- matrix(1:9, byrow = TRUE, nrow = 3)
 mat[2,3]
 matrix(1:12, byrow = FALSE, nrow = 4)
 
+matrix(seq(1, 25, 1), byrow = TRUE, nrow = 5)
+
+# последовательности
 seq(from = 0, to = 1, by = 0.2)
 seq(from = 0, to = 1, length.out = 100)
 
+# генератор (псевдо)случайных чисел
 set.seed(1234567)
 rnorm(n = 9, mean = 0, sd = 1)
 set.seed(1234567)
-matrix(rnorm(n = 9, mean = 0, sd = 1), byrow = TRUE, nrow = 3)
-
-
-
+myMatrix <- 
+  matrix(rnorm(n = 9, mean = 0, sd = 1), 
+         byrow = TRUE, nrow = 3)
+# где мы находимся в файловой системе
+# и как изменить рабочую директорию
+getwd()
+setwd('d:/YandexDisk/ИВПРАН/R forecasts/2024')
+# write.csv, read.csv
+write.csv(x = myMatrix, file = 'myMatrix.txt')
+read.csv(file = 'myMatrix.txt')
---
+++ class/class03.R
@@ -0,0 +1,76 @@
+Sys.setlocale("LC_ALL","Russian")
+library(ggplot2)
+library(readxl)
+
+getwd()
+setwd('data/baikal')
+# список файлов из рабочей директории определенного расширения
+list.files()
+xls_files <- list.files(pattern = '*.xls')
+xls_files
+
+# считываем один из файлов с учетом его структуры и требуемых столбцов
+prog_apr <- read_xls(xls_files[7], skip = 10,  
+                     col_names = c('year', 'pred', 'pred1', 'obs'), 
+                     col_types = c('numeric', 'numeric', 'skip', 'numeric'))
+
+# вариант 1: делаем считывание всех файлов в цикле с добавлением в пустой датафрейм по одному
+prog_df <- data.frame()
+for (x in xls_files){
+  print(x)
+  df <- read_xls(x, skip = 10, 
+                 col_names = c('year', 'pred', 'pred1', 'obs'), 
+                 col_types = c('numeric', 'numeric', 'skip', 'numeric'))
+  print(dim(df))
+  prog_df <- rbind(prog_df, df)
+}
+# добавляем столбец с названиями месяцев
+prog_df$month <- rep(month.abb[c(1, 10:12, 2:9)], each = 54)
+prog_df$month <- factor(prog_df$month, levels = month.abb, ordered = T)
+str(prog_df)
+
+
+# вариант 2: создаем функцию для считывания...
+read_prog <- function(x){
+  df <- read_xls(x, skip = 10, 
+                 col_names = c('year', 'pred', 'pred1', 'obs'), 
+                 col_types = c('numeric', 'numeric', 'skip', 'numeric'))
+}
+# ...и применяем ее ко всем файлам с помощью lapply
+prog_list <- lapply(xls_files, read_prog)
+names(prog_list) <- xls_files
+# после чего превращаем из списка в датафрейм
+prog_list_df <- do.call(what = rbind, args = prog_list)
+prog_list_df$month <- rownames(prog_list_df)
+rownames(prog_list_df) <- NULL
+prog_list_df$month <- substr(x = prog_list_df$month,
+       start = regexpr("[0-9]",prog_list_df$month), 
+      stop = regexpr(".xls",prog_list_df$month)-1)
+summary(prog_df)
+save(prog_df, file = 'prog_df.RData')
+
+
+# визуальная оценка 
+library(ggplot2)
+# все месяцы отдельно
+ggplot(prog_list_df, aes(obs, pred, col=month)) + 
+  geom_smooth(method = 'lm', formula = y ~ x, se = F) + 
+  geom_point() + facet_wrap(month~., scales = 'free') + geom_abline()
+
+# линии
+ggplot(prog_df, aes(x = year)) + 
+  geom_line(aes(y=obs, col='Наблюдения'), size=2) + 
+  geom_line(aes(y=pred, col='Прогноз'), linetype='dashed') + 
+  # facet_wrap(.~month, scales = 'free_y') + 
+  labs(x='Год', y=expression('Приток, м'^3*'/с'), col='Приток')
+# точки
+ggplot(prog_df, aes(x=obs, y=pred, col=month)) + geom_point(size=3) + 
+  geom_smooth(aes(group=1),method = 'lm', formula = y~x, show.legend = F, se = F) + 
+  geom_abline() + xlim(-1000, 10000) + ylim(-1000, 10000)
+# точки по месяцам
+ggplot(prog_df, aes(x=obs, y=pred, col=month)) + geom_point(size=3) + 
+  geom_smooth(aes(group=1),method = 'lm', formula = y~x, show.legend = F, se = F) + 
+  geom_abline() + facet_wrap(.~month, scales = 'free')
+
+
+
